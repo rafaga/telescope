3e17281968faa30ef1b5d100140d6106cda6fbb3
Rafael Amador Galván <rafael.amador@gmail.com>
2023-05-08 14:58:29 -0500

    added data.rs

---
+++ src/data.rs
@@ -0,0 +1,15 @@
+
+struct AppData{
+    pub scope:Vec<String>,
+    pub secret_key:&str,
+    pub clint_id:&str,
+}
+
+impl AppData{
+    fn new() -> Self{
+        scope = [""];
+        token = "";
+        token2 = "";
+    }   
+    
+ }
