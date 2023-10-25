ceef523baf6c7b7074d5669ef434175611a1a3fd
Rafael Amador Galván <rafael.amador@gmail.com>
2023-10-25 12:10:53 -0600

    enable puffin as feature

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,43 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+#[cfg(feature = "puffin")]
+fn start_puffin_server() {
+    puffin::set_scopes_on(true); // tell puffin to collect data
+
+    match puffin_http::Server::new("0.0.0.0:8585") {
+        Ok(puffin_server) => {
+            eprintln!("Run:  cargo install puffin_viewer && puffin_viewer --url 127.0.0.1:8585");
+
+            std::process::Command::new("puffin_viewer")
+                .arg("--url")
+                .arg("127.0.0.1:8585")
+                .spawn()
+                .ok();
+
+            // We can store the server if we want, but in this case we just want
+            // it to keep running. Dropping it closes the server, so let's not drop it!
+            #[allow(clippy::mem_forget)]
+            std::mem::forget(puffin_server);
+        }
+        Err(err) => {
+            eprintln!("Failed to start puffin server: {err}");
+        }
+    };
+}
+
+
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
     // Log to stdout (if you run with `RUST_LOG=debug`).
     tracing_subscriber::fmt::init();
+
+    #[cfg(feature = "puffin")]
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    #[cfg(feature = "puffin")]
+    start_puffin_server(); // NOTE: you may only want to call this if the users specifies some flag or clicks a button!
+    
     let native_options = eframe::NativeOptions::default();
     let _result = eframe::run_native(
         "Telescope",
