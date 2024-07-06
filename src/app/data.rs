043318047133e1e994096b307418bb96b48f1486
Rafael Amador <rafael.amador@gmail.com>
2024-07-06 13:22:16 -0600

    Appdata struct its now populated with the application information except for the key and the client id, now to compile you should have two environmental variables defined TELESCOPE_SECRET_KEY and TELESCOPE_CLIENT_ID

--- src/app/data.rs
+++ src/app/data.rs
@@ -9,11 +9,20 @@ pub struct AppData<'a> {
 impl<'a> AppData<'a> {
     pub fn new() -> Self {
         AppData {
-            scope: vec![""],
-            secret_key: String::new(),
-            client_id: String::new(),
-            url: String::new(),
-            user_agent: String::new(),
+            scope: vec![
+                "publicData",
+                "esi-location.read_location.v1",
+                "esi-clones.read_clones.v1",
+                "esi-characters.read_contacts.v1",
+                "esi-ui.write_waypoint.v1",
+                "esi-location.read_online.v1",
+                "esi-corporations.read_standings.v1",
+                "esi-alliances.read_contacts.v1",
+            ],
+            secret_key: std::env::var("TELESCOPE_SECRET_KEY").expect("No ESI Secret Key its configured"),
+            client_id: std::env::var("TELESCOPE_CLIENT_ID").expect("No ESI Client Id its configured"),
+            url: String::from("http://localhost:56123/login"),
+            user_agent: String::from("telescope/dev"),
         }
     }
 }
