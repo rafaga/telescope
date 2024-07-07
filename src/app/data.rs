22a1590d70366e957c5243123df67095c47d7c7e
Rafael Amador <rafael.amador@gmail.com>
2024-07-07 15:20:52 -0600

    merging test changes into master

--- src/app/data.rs
+++ src/app/data.rs
@@ -1,19 +1,28 @@
 pub struct AppData<'a> {
     pub user_agent: String,
     pub scope: Vec<&'a str>,
-    pub secret_key: String,
-    pub client_id: String,
+    pub secret_key: &'a str,
+    pub client_id: &'a str,
     pub url: String,
 }
 
 impl<'a> AppData<'a> {
     pub fn new() -> Self {
         AppData {
-            scope: vec![""],
-            secret_key: String::new(),
-            client_id: String::new(),
-            url: String::new(),
-            user_agent: String::new(),
+            scope: vec![
+                "publicData",
+                "esi-location.read_location.v1",
+                "esi-clones.read_clones.v1",
+                "esi-characters.read_contacts.v1",
+                "esi-ui.write_waypoint.v1",
+                "esi-location.read_online.v1",
+                "esi-corporations.read_standings.v1",
+                "esi-alliances.read_contacts.v1",
+            ],
+            secret_key: option_env!("TELESCOPE_SECRET_KEY").expect("ESI Secret Key it is undefined, add it to Cargo.toml in env section."),
+            client_id: option_env!("TELESCOPE_CLIENT_ID").expect("ESI Client Id it is undefined, add it to Cargo.toml in env section"),
+            url: String::from("http://localhost:56123/login"),
+            user_agent: String::from("telescope/dev"),
         }
     }
 }
